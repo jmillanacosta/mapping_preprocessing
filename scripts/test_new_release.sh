451f2aa188a3c7939755737f68ce365a46fe24b6
Javier Millán Acosta <83466805+jmillanacosta@users.noreply.github.com>
2024-04-11 15:07:51 +0200

    Tries to fix issue with processing

--- scripts/test_new_release.sh
+++ scripts/test_new_release.sh
@@ -5,6 +5,29 @@
 # Author: Javier Millan Acosta
 # Date: April 2024
 
+# Function to download files and store their names
+download_files() {
+    local urls=("$@")
+    for url in "${urls[@]}"; do
+        wget "$url"
+        local filename=$(basename "$url")
+        downloaded_files+=("$filename")
+    done
+}
+
+# Function to move downloaded files to the data directory
+move_files_to_data() {
+    for file in "${downloaded_files[@]}"; do
+        mv "$file" "datasources/$source/data/"
+    done
+}
+
+# Function to create temp folders and perform common setup
+setup_common() {
+    mkdir -p "datasources/$source/data"
+    echo "$DATE_NEW=$DATE_NEW" >> $GITHUB_ENV
+}
+
 # Check if the source argument is provided
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <source> (chebi | hgnc | hmdb | ncbi | uniprot)"
@@ -25,73 +48,61 @@ fi
 echo "$DATE_NEW=$DATE_NEW" >> $GITHUB_ENV
 
 # Create temp. folders to store the data in
-mkdir -p "datasources/$source/data"
+setup_common
 
 # Access the source to retrieve the latest release date
 echo "Accessing the $source archive"
 case $source in
     "chebi")
-        echo "RELEASE_NUMBER=$RELEASE_NUMBER" >> $GITHUB_ENV
-        echo "CURRENT_RELEASE_NUMBER=$CURRENT_RELEASE_NUMBER" >> $GITHUB_ENV
-        url_release="https://ftp.ebi.ac.uk/pub/databases/chebi/archive/rel$RELEASE_NUMBER/SDF/"
-        echo "URL_RELEASE=$url_release" >> $GITHUB_ENV
-        wget "https://ftp.ebi.ac.uk/pub/databases/chebi/archive/rel${RELEASE_NUMBER}/SDF/ChEBI_complete_3star.sdf.gz"
+        download_files "https://ftp.ebi.ac.uk/pub/databases/chebi/archive/rel${RELEASE_NUMBER}/SDF/ChEBI_complete_3star.sdf.gz"
         gunzip ChEBI_complete_3star.sdf.gz
-        ls
-        mkdir -p mapping_preprocessing/datasources/chebi/data
+        move_files_to_data
+        mkdir -p "mapping_preprocessing/datasources/chebi/data"
         inputFile="ChEBI_complete_3star.sdf" 
-        mkdir new
         outputDir="datasources/chebi/recentData/"
         ;;
     "hgnc")
-        echo "$COMPLETE_NEW=$COMPLETE_NEW" >> $GITHUB_ENV
-        echo "$WITHDRAWN_NEW=$WITHDRAWN_NEW" >> $GITHUB_ENV
-        wget "https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/archive/quarterly/tsv/${WITHDRAWN_NEW}" \
-             "https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/archive/quarterly/tsv/${COMPLETE_NEW}"
-        mv "$WITHDRAWN_NEW" "$COMPLETE_NEW" datasources/hgnc/data
-        ls -trlh datasources/hgnc/data
+        download_files "https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/archive/quarterly/tsv/${WITHDRAWN_NEW}" \
+                       "https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/archive/quarterly/tsv/${COMPLETE_NEW}"
+        move_files_to_data
         sourceVersion=$DATE_NEW
         complete="datasources/hgnc/data/${COMPLETE_NEW}" 
         withdrawn="datasources/hgnc/data/${WITHDRAWN_NEW}" 
         ;;
     "hmdb")
         sudo apt-get install -y xml-twig-tools
-        wget "http://www.hmdb.ca/system/downloads/current/hmdb_metabolites.zip"
+        download_files "http://www.hmdb.ca/system/downloads/current/hmdb_metabolites.zip"
         unzip hmdb_metabolites.zip
-        mkdir -p hmdb
         mv hmdb_metabolites.xml hmdb
         cd hmdb
         xml_split -v -l 1 hmdb_metabolites.xml
         rm hmdb_metabolites.xml
         cd ../
         zip -r hmdb_metabolites_split.zip hmdb
-        to_check_from_zenodo=$(grep -E '^to_check_from_zenodo=' datasources/hmdb/config | cut -d'=' -f2)
         inputFile=hmdb_metabolites_split.zip
         mkdir datasources/hmdb/recentData/
         outputDir="datasources/hmdb/recentData/"
         ;;
     "ncbi")
-        wget "https://ftp.ncbi.nih.gov/gene/DATA/gene_info.gz" \
-             "https://ftp.ncbi.nih.gov/gene/DATA/gene_history.gz"
-        mv gene_info.gz gene_history.gz datasources/ncbi/data
-        ls -trlh datasources/ncbi/data
+        download_files "https://ftp.ncbi.nih.gov/gene/DATA/gene_info.gz" \
+                       "https://ftp.ncbi.nih.gov/gene/DATA/gene_history.gz"
+        move_files_to_data
         sourceVersion=$DATE_NEW
-        gene_history="data/gene_history.gz" 
-        gene_info="data/gene_info.gz" 
+        gene_history="datasources/ncbi/data/gene_history.gz" 
+        gene_info="datasources/ncbi/data/gene_info.gz" 
         ;;
     "uniprot")
         UNIPROT_SPROT_NEW="uniprot_sprot.fasta.gz"
         SEC_AC_NEW="sec_ac.txt"
         DELAC_SP_NEW="delac_sp.txt"
-        wget "https://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/${UNIPROT_SPROT_NEW}" \
-             "https://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/docs/${SEC_AC_NEW}" \
-             "https://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/docs/${DELAC_SP_NEW}"
-        mv "$DELAC_SP_NEW" "$SEC_AC_NEW" "$UNIPROT_SPROT_NEW" datasources/uniprot/data
-        ls -trlh datasources/uniprot/data
+        download_files "https://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/${UNIPROT_SPROT_NEW}" \
+                       "https://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/docs/${SEC_AC_NEW}" \
+                       "https://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/docs/${DELAC_SP_NEW}"
+        move_files_to_data
         sourceVersion=$DATE_NEW
-        uniprot_sprot=$(echo datasources/uniprot/data/uniprot_sprot.fasta.gz)
-        sec_ac=$(echo datasources/uniprot/data/sec_ac.txt)
-        delac_sp=$(echo datasources/uniprot/data/delac_sp.txt)         
+        uniprot_sprot="datasources/uniprot/data/uniprot_sprot.fasta.gz"
+        sec_ac="datasources/uniprot/data/sec_ac.txt"
+        delac_sp="datasources/uniprot/data/delac_sp.txt"         
         ;;
     *)
         echo "Invalid source: $source"
